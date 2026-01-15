9fc8daf51b21b5c88303477fc31c8e798ac4724e
jadhavpandu <149279183+jadhavpandu@users.noreply.github.com>
2026-01-15 11:33:36 +0000

    first api check on POSTMAN

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app = express();
 //Converts JSON request body into JavaScript object (req.body)
 app.use(express.json());
 
+app.use("/home",(req,res)=>{
+    res.send("this is the jsut for check");
+})
 app.listen(3000,(req,res)=>{
     console.log("listen at port 3000");
 })
